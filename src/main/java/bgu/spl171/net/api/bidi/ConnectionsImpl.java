1771cb37634ad08002596db9048c73e3fa068e21
elad <eladsham@post.bgu.ac.il>
2017-01-11 17:49:49 +0200

    added bidi

--- src/main/java/bgu/spl171/net/api/bidi/ConnectionsImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/ConnectionsImpl.java
@@ -1,9 +1,12 @@
 package bgu.spl171.net.api.bidi;
+import bgu.spl171.net.srv.bidi.ConnectionHandler;
+import java.util.HashMap;
 
 /**
  * Created by nir on 11/01/17.
  */
 public class ConnectionsImpl<T> implements Connections<T> {
+    private HashMap<Integer, ConnectionHandler> connections;
     @Override
     public boolean send(int connectionId, T msg) {
         return false;
