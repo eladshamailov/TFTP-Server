4b2a7909ee080c4244555bf79b5ceff80e72c0c5
elad <eladsham@post.bgu.ac.il>
2017-01-11 17:29:48 +0200

    added method to impl

--- src/main/java/bgu/spl171/net/api/bidi/ConnectionsImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/ConnectionsImpl.java
@@ -4,4 +4,18 @@ package bgu.spl171.net.api.bidi;
  * Created by nir on 11/01/17.
  */
 public class ConnectionsImpl<T> implements Connections<T> {
+    @Override
+    public boolean send(int connectionId, T msg) {
+        return false;
+    }
+
+    @Override
+    public void broadcast(T msg) {
+
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+
+    }
 }
