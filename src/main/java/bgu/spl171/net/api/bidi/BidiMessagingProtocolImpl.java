2d2705d606d0e9c12833ff1d2d8172ddcb0e4cec
elad <eladsham@post.bgu.ac.il>
2017-01-12 16:32:31 +0200

    added packets

--- src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -1,5 +1,6 @@
 package bgu.spl171.net.api.bidi;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -8,6 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<T> {
     private Connections<T> connections;
     private int connectionId;
+    private static ConcurrentHashMap<Integer, String> activeClients=new ConcurrentHashMap<>();
     @Override
     public void start(int connectionId, Connections<T> connections) {
         this.connectionId=connectionId;
@@ -16,7 +18,6 @@ public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<T> {
 
     @Override
     public void process(T message) {
-
     }
 
     @Override
---
+++ src/main/java/bgu/spl171/net/packets/Packet.java
@@ -0,0 +1,7 @@
+package bgu.spl171.net.packets;
+
+/**
+ * Created by elad on 1/12/17.
+ */
+public class Packet {
+}
