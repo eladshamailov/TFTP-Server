ffee3f81786794218b70d6a9a76a4dc815d2088e
elad <eladsham@post.bgu.ac.il>
2017-01-12 18:45:13 +0200

    added packets

--- src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -1,23 +1,26 @@
 package bgu.spl171.net.api.bidi;
 
+import bgu.spl171.net.packets.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by elad on 1/12/17.
  */
-public class BidiMessagingProtocolImpl<T> implements BidiMessagingProtocol<T> {
-    private Connections<T> connections;
+public class BidiMessagingProtocolImpl<Packet> implements BidiMessagingProtocol<Packet> {
+    private Connections<Packet> connections;
     private int connectionId;
     private static ConcurrentHashMap<Integer, String> activeClients=new ConcurrentHashMap<>();
     @Override
-    public void start(int connectionId, Connections<T> connections) {
+    public void start(int connectionId, Connections<Packet> connections) {
         this.connectionId=connectionId;
         this.connections=connections;
     }
 
     @Override
-    public void process(T message) {
+    public void process(Packet message) {
+        if (message==null){
+            connections.send(connectionId,new ERROR((short)4,(bgu.spl171.net.packets.Packet)"Illegal TFTP operation–Unknown Opcode"));
+        }
     }
 
     @Override
