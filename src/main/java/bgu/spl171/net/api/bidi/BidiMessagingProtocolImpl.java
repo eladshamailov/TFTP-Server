3f33a7fa8e34247df0f6c13524db51a0120e34da
nir <abelnir90@gmail.com>
2017-01-15 12:59:28 +0200

    decoder class

--- src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Created by elad on 1/12/17.
  */
-public class BidiMessagingProtocolImpl<Packet> implements BidiMessagingProtocol<Packet> {
+public class BidiMessagingProtocolImpl implements BidiMessagingProtocol<Packet> {
     private Connections<Packet> connections;
     private int connectionId;
     private static ConcurrentHashMap<Integer, String> activeClients=new ConcurrentHashMap<>();
@@ -20,6 +20,27 @@ public class BidiMessagingProtocolImpl<Packet> implements BidiMessagingProtocol<
     public void process(Packet message) {
         if (message==null){
             connections.send(connectionId, new ERROR((short) 4, "Illegal TFTP operation–Unknown Opcode"));
+        }
+        else{
+            if(message.getOpCode()==7){//if the user chose to connect LOGCQ
+                if(activeClients.containsValue((((LOGRQ)message).getuserName()))) {
+                    connections.send(connectionId, new ERROR((short) 7, "User already logged in–Login username already connected"));
+                }
+                else{
+                    activeClients.put(connectionId,((LOGRQ)message).getuserName());
+                    connections.send(connectionId,new ACK((short)0));
+                }
+            }
+            else{
+                if(activeClients.containsKey(connectionId)){
+                    //TODO:add the cases
+                    switch (message.getOpCode()){
+                    }
+                }
+                else{
+                    connections.send(connectionId, new ERROR((short) 6, "User not logged in–Any opcode received before Login completes"));
+                }
+            }
         }
     }
 
