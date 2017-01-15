f0c2c366f17991282c1c8ab831e28ca6060f40c2
elad <eladsham@post.bgu.ac.il>
2017-01-15 13:04:14 +0200

    adding bidimessagingprotocolimpl

--- src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
+++ src/main/java/bgu/spl171/net/api/bidi/BidiMessagingProtocolImpl.java
@@ -1,49 +1,112 @@
 package bgu.spl171.net.api.bidi;
 
 import bgu.spl171.net.packets.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Created by elad on 1/12/17.
  */
 public class BidiMessagingProtocolImpl implements BidiMessagingProtocol<Packet> {
     private Connections<Packet> connections;
-    private int connectionId;
+    private int connId;
     private static ConcurrentHashMap<Integer, String> activeClients=new ConcurrentHashMap<>();
+    private ConcurrentLinkedDeque<String> writing;
+    private LinkedBlockingDeque<DATA> readData;
+    private LinkedBlockingDeque<byte[]> writeData;
+
     @Override
     public void start(int connectionId, Connections<Packet> connections) {
-        this.connectionId=connectionId;
+        this.connId =connectionId;
         this.connections=connections;
+        writing=new ConcurrentLinkedDeque<>();
+        readData=new LinkedBlockingDeque<>();
+        writeData=new LinkedBlockingDeque<>();
     }
 
     @Override
     public void process(Packet message) {
-        if (message==null){
-            connections.send(connectionId, new ERROR((short) 4, "Illegal TFTP operation–Unknown Opcode"));
-        }
-        else{
-            if(message.getOpCode()==7){//if the user chose to connect LOGCQ
-                if(activeClients.containsValue((((LOGRQ)message).getuserName()))) {
-                    connections.send(connectionId, new ERROR((short) 7, "User already logged in–Login username already connected"));
-                }
-                else{
-                    activeClients.put(connectionId,((LOGRQ)message).getuserName());
-                    connections.send(connectionId,new ACK((short)0));
-                }
-            }
-            else{
-                if(activeClients.containsKey(connectionId)){
-                    switch (message.getOpCode()){
+        boolean loggedSuccessfully = handleLOGRQ(message);
+        if (loggedSuccessfully) {
+            switch (message.getOpCode()) {
+                case 1: {
+                    RRQ rrqPack = (RRQ) message;
+                    try {
+                        Path path = Paths.get("/Files", rrqPack.getName());
+                        if (!isWriting(rrqPack.getName())) {
+                            byte[] data = Files.readAllBytes(path);
+                            sendData(data);
+                        }
                     }
+                    catch (FileNotFoundException exp) {
+                        connections.send(connId, new ERROR((short) 1));
+                    }
+                    catch (IOException exp) {
+                        connections.send(connId, new ERROR((short) 2));
+                    }
+                    break;
                 }
-                else{
-                    connections.send(connectionId, new ERROR((short) 6, "User not logged in–Any opcode received before Login completes"));
+                case 2:{
+
                 }
             }
         }
     }
+
+    /**
+     * handles the login process
+     * @param message
+     * @return
+     */
+    private boolean handleLOGRQ(Packet message){
+        boolean ans=false;
+        if (message==null){
+            connections.send(connId, new ERROR((short) 4));
+        }
+        else {
+            if(message.getOpCode()==7){//if the user chose to connect LOGCQ
+                if(activeClients.containsValue((((LOGRQ)message).getuserName()))) {
+                    connections.send(connId, new ERROR((short) 7));
+                }
+                else{
+                    activeClients.put(connId,((LOGRQ)message).getuserName());
+                    connections.send(connId,new ACK((short)0));
+                }
+            }
+            else{
+                if(!(activeClients.containsKey(connId))){
+                    connections.send(connId, new ERROR((short)6));
+                }
+                else{
+                    ans=true;
+                }
+            }
+        }
+        return ans;
+    }
     @Override
     public boolean shouldTerminate() {
         return false;
+    }
+
+    private boolean isWriting(String str){
+        boolean found=false;
+        Iterator<String> it = writing.iterator();
+        while (it.hasNext()&&!found){
+            if((it.next()).equals(str))
+                found=true;
+        }
+        return found;
+    }
+    private void sendData(byte[] data){
+
     }
 }
--- src/main/java/bgu/spl171/net/packets/ERROR.java
+++ src/main/java/bgu/spl171/net/packets/ERROR.java
@@ -6,10 +6,47 @@ package bgu.spl171.net.packets;
 public class ERROR extends Packet {
     private short errorCode;
     private String errorMassage;
-    public ERROR(short errorCode,String errorMassage) {
+    public ERROR(short errorCode) {
         super((short)5);
         this.errorCode=errorCode;
-        this.errorMassage=errorMassage;
+        switch (errorCode){
+            case 0: {
+                this.errorMassage = "Not defined, see error message (if any).";
+                break;
+            }
+            case 1: {
+                this.errorMassage = "File not found–RRQ\\DELRQ of non-existing file";
+                break;
+            }
+            case 2:{
+                this.errorMassage = "Access violation–File cannot be written, read or deleted.";
+                break;
+            }
+            case 3:{
+                this.errorMassage = "Access violation–File cannot be written, read or deleted.Disk full or allocation exceeded–No room in disk.";
+                break;
+            }
+            case 4:{
+                this.errorMassage = "Illegal TFTP operation–Unknown Opcode.";
+                break;
+            }
+            case 5:{
+                this.errorMassage = "File already exists–File name exists on WRQ.";
+                break;
+            }
+            case 6:{
+                this.errorMassage = "User not logged in–Any opcode received before Login completes.";
+                break;
+            }
+            case 7:{
+                this.errorMassage = "User already logged in–Login usernamealready connected.";
+                break;
+            }
+            default:{
+                this.errorMassage = "Not defined, see error message (if any).";
+                break;
+            }
+        }
     }
 
     public short getErrorCode() {
